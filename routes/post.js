b062edf34b78e7c8c6e67826bb1051b9c924199a
suyeonJo <tndus8334@naver.com>
2021-04-25 21:26:20 +0900

    s3 재업로드 준비

--- routes/post.js
+++ routes/post.js
@@ -18,15 +18,15 @@ try {
 }
 
 AWS.config.update({
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: 'ap-northeast-2', //서울
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2', //서울
 });
 // 6강 multer array single none fields
 const upload = multer({
     storage: multerS3({
         s3: new AWS.S3(),
-        bucket: 'suyeojo',
+        bucket: 'suyeonjo',
         key(req, file, cb){
             cb(null, `original/'${Date.now()}${path.basename(file.originalname)}`);
         },
@@ -42,37 +42,37 @@ router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
 });
 
 //게시글 작성, 이미지는 이미 업로드 되어있고, 더이상 업로드하지않는다.
-router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
-    try {
-        const post = await Post.create(
-            {content: req.body.content, img: req.body.url, UserId: req.user.id}
-        );
-        const hashtags = req
-            .body
-            .content
-            .match(/#[^\s#]*/g); //정규 표현식을 활용한 해쉬태그 추출
-        // [#노드, #익스프레스] [노드, 익스프레스] [findOrCreate(노드), findOrCreate(익스프레스)] [[해시태그,
-        // false]. [해시태그, true]]
-        if (hashtags) {
-            const result = await Promise.all(hashtags.map(tag => {
-                return Hashtag.findOrCreate({
-                    where: {
-                        title: tag
-                            .slice(1)
-                            .toLowerCase()
-                    }
-                })
-            }),);
-            console.log(result);
-            await post.addHashtags(result.map(r => r[0]));
-            // addFollowings[]
-        }
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
+// router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
+//     try {
+//         const post = await Post.create(
+//             {content: req.body.content, img: req.body.url, UserId: req.user.id}
+//         );
+//         const hashtags = req
+//             .body
+//             .content
+//             .match(/#[^\s#]*/g); //정규 표현식을 활용한 해쉬태그 추출
+//         // [#노드, #익스프레스] [노드, 익스프레스] [findOrCreate(노드), findOrCreate(익스프레스)] [[해시태그,
+//         // false]. [해시태그, true]]
+//         if (hashtags) {
+//             const result = await Promise.all(hashtags.map(tag => {
+//                 return Hashtag.findOrCreate({
+//                     where: {
+//                         title: tag
+//                             .slice(1)
+//                             .toLowerCase()
+//                     }
+//                 })
+//             }),);
+//             console.log(result);
+//             await post.addHashtags(result.map(r => r[0]));
+//             // addFollowings[]
+//         }
+//         res.redirect('/');
+//     } catch (error) {
+//         console.error(error);
+//         next(error);
+//     }
+// });
 
 // 실제 파일은 업로드에 있는데, 요청주소는 img 이것을 맞혀주기 위해 스태틱을 이용함
 // =========================================================================================
